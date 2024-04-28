8f5a755281e166765ade52b8b4cf3a8023505cce
unknown <24026510@mvula.univen.ac.za>
2024-04-28 16:04:48 +0200

    first commit

---
+++ max.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class max {
+    public static void main(String[] args) {
+String result;
+    result = JOptionPane.showInputDialog(null ,"Who Is Your Love?");
+    JOptionPane.showMessageDialog(null ,"I Love You," + result + "!");
+
+    }
+}
